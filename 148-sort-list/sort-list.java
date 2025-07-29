c9c9b21d290ddb60f9a667cbeabf32a563be4adb
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2025-07-30 01:51:06 +0530

    Time: 9 ms (96.05%) | Memory: 56.8 MB (54.61%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -9,39 +9,104 @@
  * }
  */
 class Solution {
+
+    public static ListNode mergeTwoSortedLinkedLists(ListNode list1, ListNode list2) {
+            // Create a dummy node to serve
+            // as the head of the merged list
+            ListNode dummyNode = new ListNode(-1);
+            ListNode temp = dummyNode;
+
+            // Traverse both lists simultaneously
+            while (list1 != null && list2 != null) {
+                // Compare elements of both lists and
+                // link the smaller node to the merged list
+                if (list1.val <= list2.val) {
+                    temp.next = list1;
+                    list1 = list1.next;
+                } else {
+                    temp.next = list2;
+                    list2 = list2.next;
+                }
+                // Move the temporary pointer
+                // to the next node
+                temp = temp.next; 
+            }
+
+            if (list1 != null) {
+                temp.next = list1;
+            } else {
+                temp.next = list2;
+            }
+            // Return the merged list starting 
+            // from the next of the dummy node
+            return dummyNode.next;
+    }
+
+    public static ListNode findMiddle(ListNode head){
+            if (head == null || head.next == null) {
+                return head;
+            }
+
+            ListNode slow = head;
+            ListNode fast = head.next;
+
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+    }
+
     public ListNode sortList(ListNode head) {
-        // Create a list to
-        // store node values
-        List<Integer> arr = new ArrayList<>();
-        
-        // Temporary pointer to
-        // traverse the linked list
-        ListNode temp = head;
-        
-        // Traverse the linked list and
-        // store node values in the list
-        while(temp != null){
-            arr.add(temp.val);
-            temp = temp.next;
+
+        if (head == null || head.next == null) {
+            return head;
         }
         
-        // Sort the list
-        // containing node values
-        Collections.sort(arr);
+        // Find the middle of the list
+        // using the findMiddle function
+        ListNode middle = findMiddle(head);
         
-        // Reassign sorted values to
-        // the linked list nodes
-        temp = head;
-        for(int i = 0; i < arr.size(); i++){
-            // Update the node's data
-            // with the sorted values
-            temp.val = arr.get(i); 
-            // Move to the next node
-            temp = temp.next; 
-        }
+        // Divide the list into two halves
+        ListNode right = middle.next;
+        middle.next = null;
+        ListNode left = head;
         
-        // Return the head of the
-        // sorted linked list
-        return head; 
+        // Recursively sort the left and right halves
+        left = sortList(left);
+        right = sortList(right);
+        
+        // Merge the sorted halves using the
+        return mergeTwoSortedLinkedLists(left, right);
     }
 }
+
+// class Solution {
+//     public ListNode sortList(ListNode head) {
+//         List<Integer> arr = new ArrayList<>();
+        
+//         ListNode temp = head;
+        
+//         while(temp != null){
+//             arr.add(temp.val);
+//             temp = temp.next;
+//         }
+        
+//         // Sort the list
+//         // containing node values
+//         Collections.sort(arr);
+        
+//         temp = head;
+//         for(int i = 0; i < arr.size(); i++){
+//             // Update the node's data
+//             // with the sorted values
+//             temp.val = arr.get(i); 
+//             // Move to the next node
+//             temp = temp.next; 
+//         }
+        
+//         // Return the head of the
+//         // sorted linked list
+//         return head; 
+//     }
+// }
