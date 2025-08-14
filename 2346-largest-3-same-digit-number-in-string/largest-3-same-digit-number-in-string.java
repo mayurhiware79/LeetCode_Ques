ca0bed7fe49e3e434fb8f363129071c042158571
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2025-08-14 23:46:14 +0530

    Time: 2 ms (81.68%) | Memory: 41.8 MB (92.25%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
@@ -1,12 +1,31 @@
 class Solution {
     public String largestGoodInteger(String num) {
-        
-        for(int d =9; d >= 0; d--){
-            String pattern = ""+d+d+d;
-            if(num.contains(pattern)){
-                return pattern;
+        int result =-1;
+        for(int i=0; i + 2 < num.length(); i++){
+            if(num.charAt(i)==num.charAt(i+1) && num.charAt(i)==num.charAt(i+2)){
+                result = Math.max(result, num.charAt(i)-'0');
             }
         }
-        return "";
+
+        StringBuilder builder = new StringBuilder();
+        for(int i=0;i<3;i++){
+            builder.append((char)(48+ result));
+        }
+        return result ==-1  ? "" : builder.toString();
     }
 }
+
+
+// BRUTE FORCE
+// class Solution {
+//     public String largestGoodInteger(String num) {
+        
+//         for(int d =9; d >= 0; d--){
+//             String pattern = ""+d+d+d;
+//             if(num.contains(pattern)){
+//                 return pattern;
+//             }
+//         }
+//         return "";
+//     }
+// }
