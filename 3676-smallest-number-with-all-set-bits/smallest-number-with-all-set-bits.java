3a296c0842cad9c37b87afd5cc72948cf77d5d92
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2025-11-01 19:45:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (5.72%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
@@ -1,20 +1,20 @@
 class Solution {
     public int smallestNumber(int n) {
 
-        int[] allOnes = {1, 3, 7, 15, 31, 63, 127, 255, 511, 1023, 2047};
-        for(int x:allOnes){
-            if(x >= n) return x;
-        }
+        // int[] allOnes = {1, 3, 7, 15, 31, 63, 127, 255, 511, 1023, 2047};
+        // for(int x:allOnes){
+        //     if(x >= n) return x;
+        // }
 
-        return -1;
+        // return -1;
 
         // 1 << k = 2^k
         //1(1) 3(11) 5(111) ===> 2^k - 1 pattern observed
-        // int x = 1;
-        // while( (1 << x) - 1 < n){
-        //     x++;
-        // }
-        // return (1 << x) -1;
+        int x = 1;
+        while( (1 << x) - 1 < n){
+            x++;
+        }
+        return (1 << x) -1;
         
     }
 }
