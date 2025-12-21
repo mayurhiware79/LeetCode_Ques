b4b399f0a4fbe523afba140b0bcaa46228ec2688
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2025-12-22 00:52:11 +0530

    Time: 1 ms (78.08%) | Memory: 43.4 MB (60.27%) - LeetSync

---
+++ 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int n = strs.length;
+        int m = strs[0].length();
+
+        int deletions = 0;
+        //sorted[i] neabs strs[i] < strs[i+1] already confirmed
+        boolean[] sorted = new boolean[n-1];
+        
+        for(int col=0; col < m; col++){
+            boolean badCol = false;
+
+            //check if this column violates order
+            for(int i=0; i< n-1; i++){
+                if(!sorted[i]){
+                    char a = strs[i].charAt(col);
+                    char b = strs[i+1].charAt(col);
+                    if(a > b){
+                        badCol = true;
+                        break;
+                    }
+                }
+            }
+
+            if(badCol){
+                deletions++;
+                continue;
+            }
+
+            //otherwise, update sorted pairs
+            for(int i=0; i< n-1; i++){
+                if(!sorted[i]){
+                    char a = strs[i].charAt(col);
+                    char b = strs[i+1].charAt(col);
+                    if(a < b){
+                        sorted[i] = true;
+                    }
+                }
+            }
+        }
+
+        return deletions;
+    }
+}
