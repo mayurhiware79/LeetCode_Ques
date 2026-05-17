0b9c3e484c5066317bffcc495a57479606017588
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2026-05-17 20:08:07 +0530

    Time: 2 ms (82.42%) | Memory: 52.9 MB (33.20%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        boolean[] visited = new boolean[arr.length];
+
+        return dfs(arr, start, visited);
+    }
+
+    private boolean dfs(int[] arr, int i, boolean[] visited){
+        if(i < 0 || i>=arr.length){
+            return false;
+        }
+
+        if(visited[i]){
+            return false;
+        }
+
+        if(arr[i]==0){
+            return true;
+        }
+
+        visited[i]=true;
+
+        //explre both direction
+        return dfs(arr, i+arr[i], visited) || dfs(arr, i-arr[i], visited);
+
+    }
+}
