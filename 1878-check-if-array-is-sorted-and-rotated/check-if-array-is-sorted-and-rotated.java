784ce057704a76de933bdb4ca287c8dbabc40647
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2026-05-23 22:55:40 +0530

    Time: 2 ms (4.70%) | Memory: 43.2 MB (11.32%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+
+        int[] sorted = new int[n];
+
+        for (int r = 0; r < n; r++) {
+
+            int idx = 0;
+
+            // Copy from r to n-1
+            for (int i = r; i < n; i++) {
+                sorted[idx] = nums[i];
+                idx++;
+            }
+
+            // Copy from 0 to r-1
+            for (int i = 0; i < r; i++) {
+                sorted[idx] = nums[i];
+                idx++;
+            }
+
+            // Check if sorted
+            boolean isSorted = true;
+
+            for (int i = 0; i < n - 1; i++) {
+                if (sorted[i] > sorted[i + 1]) {
+                    isSorted = false;
+                    break;
+                }
+            }
+
+            if (isSorted) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
