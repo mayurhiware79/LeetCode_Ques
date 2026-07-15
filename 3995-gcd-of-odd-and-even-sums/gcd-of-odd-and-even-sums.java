0b211dc9ea00aa26c1ef17cd33b7db65de70e242
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2026-07-16 02:13:15 +0530

    Time: 1 ms (83.02%) | Memory: 42.5 MB (57.16%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,5 +1,51 @@
+// class Solution {
+//     public int gcdOfOddEvenSums(int n) {
+//         return n;
+//     }
+// }
+
+// class Solution {
+
+//     public int gcdOfOddEvenSums(int n) {
+
+//         int sumOdd = 0;
+//         int sumEven = 0;
+
+//         for (int i = 1; i <= n; i++) {
+//             sumOdd += (2 * i - 1);
+//             sumEven += (2 * i);
+//         }
+
+//         return gcd(sumOdd, sumEven);
+//     }
+
+//     public int gcd(int a, int b) {
+//         while (b != 0) {
+//             int temp = a % b;
+//             a = b;
+//             b = temp;
+//         }
+//         return a;
+//     }
+
+// }
+
 class Solution {
+
+    public int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
     public int gcdOfOddEvenSums(int n) {
-        return n;
+
+        int sumOdd = n * n;
+        int sumEven = n * (n + 1);
+
+        return gcd(sumOdd, sumEven);
     }
 }
