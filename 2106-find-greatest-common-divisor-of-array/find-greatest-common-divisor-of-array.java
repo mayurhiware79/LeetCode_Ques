0ecb1a370c5a5d88d11985ae7cf4bb10a357d90f
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2026-07-18 23:40:24 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (28.11%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -1,14 +1,50 @@
+// class Solution {
+//     public int findGCD(int[] nums) {
+//         Arrays.sort(nums);
+//         int a = nums[0];
+//         int b = nums[nums.length-1];
+
+//         while (b != 0) {
+//             int temp = b;
+//             b = a % b;
+//             a = temp;
+//         }
+//         return Math.abs(a); // Returns positive GCD even for negative inputs
+//     }
+
+// }
+
 class Solution {
+
     public int findGCD(int[] nums) {
-        Arrays.sort(nums);
-        int a = nums[0];
-        int b = nums[nums.length-1];
+
+        int min = nums[0];
+        int max = nums[0];
+
+        
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+
+        return gcd(min, max);
+    }
+
+    public int gcd(int a, int b) {
+
         while (b != 0) {
+
             int temp = b;
             b = a % b;
             a = temp;
         }
-        return Math.abs(a); // Returns positive GCD even for negative inputs
-    }
 
+        return a;
+    }
 }
