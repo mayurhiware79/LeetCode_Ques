71ca2ba0b0dd2e660ad1a6116bab79f66ffd4267
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2026-07-21 00:41:32 +0530

    Time: 6 ms (55.40%) | Memory: 47.3 MB (26.21%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int n = row * col;
+
+        k = k % n;
+
+        if (k != 0) {
+            reverse(grid, col, 0, n - 1);
+            reverse(grid, col, 0, k - 1);
+            reverse(grid, col, k, n - 1);
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+        for (int[] r : grid) {
+            List<Integer> rowList = new ArrayList<>();
+            for (int val : r) {
+                rowList.add(val);
+            }
+            result.add(rowList);
+        }
+        return result;
+    }
+
+    private void reverse(int[][] grid, int col, int i, int j) {
+        while (i < j) {
+            int temp = grid[i / col][i % col];
+
+            grid[i / col][i % col] = grid[j / col][j % col];
+
+            grid[j / col][j % col] = temp;
+            
+            i++;
+            j--;
+        }
+    }
+}
