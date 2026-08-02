b584e89502ff1cf793898f87aa82b48aad74e046
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2026-08-02 22:50:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (95.35%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,23 +1,29 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int n = piles.length;
-        if ((n & 1) == 0) return true; // if even size of arr then always true
-
-        int[][] dp= new int[n][n];
-        for(int[] r: dp){
-            Arrays.fill(r, -1);
-        }
-
-        return maxDiff(0, n-1, piles, dp) >= 0;
-    }
-
-    private int maxDiff(int i, int j, int[] A, int[][] dp) {
-        if(dp[i][j] != -1) return dp[i][j];
-        if(i == j) return dp[i][j] = A[i];
-
-        return dp[i][j] = Math.max(
-            A[i] - maxDiff(i+1, j, A,dp),
-            A[j] - maxDiff(i, j-1, A, dp)
-        );
+        return true;
     }
 }
+
+// class Solution {
+//     public boolean stoneGame(int[] piles) {
+//         int n = piles.length;
+//         if ((n & 1) == 0) return true; // if even size of arr then always true
+
+//         int[][] dp= new int[n][n];
+//         for(int[] r: dp){
+//             Arrays.fill(r, -1);
+//         }
+
+//         return maxDiff(0, n-1, piles, dp) >= 0;
+//     }
+
+//     private int maxDiff(int i, int j, int[] A, int[][] dp) {
+//         if(dp[i][j] != -1) return dp[i][j];
+//         if(i == j) return dp[i][j] = A[i];
+
+//         return dp[i][j] = Math.max(
+//             A[i] - maxDiff(i+1, j, A,dp),
+//             A[j] - maxDiff(i, j-1, A, dp)
+//         );
+//     }
+// }
