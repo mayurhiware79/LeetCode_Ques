d957d440a78777c52e4b996b189f49e9f99b57c6
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2026-08-06 00:45:50 +0530

    Time: 9 ms (36.46%) | Memory: 49.2 MB (43.33%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -4,30 +4,56 @@ class Solution {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
         List<int[]> ans = new ArrayList<>();
-        int n = intervals.length;
 
-        for (int i = 0; i < n; i++) {
+        for (int[] interval : intervals) {
 
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            // Skip intervals already covered
-            if (!ans.isEmpty() && end <= ans.get(ans.size() - 1)[1]) {
-                continue;
+            // If no overlap, add the interval
+            if (ans.isEmpty() || interval[0] > ans.get(ans.size() - 1)[1]) {
+                ans.add(interval);
             }
-
-            for (int j = i + 1; j < n; j++) {
-
-                if (intervals[j][0] <= end) {
-                    end = Math.max(end, intervals[j][1]);
-                } else {
-                    break;
-                }
+            // Overlap exists, merge the intervals
+            else {
+                ans.get(ans.size() - 1)[1] = Math.max(
+                    ans.get(ans.size() - 1)[1],
+                    interval[1]
+                );
             }
-
-            ans.add(new int[]{start, end});
         }
 
         return ans.toArray(new int[ans.size()][]);
     }
 }
+
+// class Solution {
+//     public int[][] merge(int[][] intervals) {
+
+//         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+//         List<int[]> ans = new ArrayList<>();
+//         int n = intervals.length;
+
+//         for (int i = 0; i < n; i++) {
+
+//             int start = intervals[i][0];
+//             int end = intervals[i][1];
+
+//             // Skip intervals already covered
+//             if (!ans.isEmpty() && end <= ans.get(ans.size() - 1)[1]) {
+//                 continue;
+//             }
+
+//             for (int j = i + 1; j < n; j++) {
+
+//                 if (intervals[j][0] <= end) {
+//                     end = Math.max(end, intervals[j][1]);
+//                 } else {
+//                     break;
+//                 }
+//             }
+
+//             ans.add(new int[]{start, end});
+//         }
+
+//         return ans.toArray(new int[ans.size()][]);
+//     }
+// }
