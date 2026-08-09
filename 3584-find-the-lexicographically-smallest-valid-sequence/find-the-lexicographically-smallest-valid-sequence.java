f53b8de3fb425784fc16b1db0fdc754227a452af
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2026-08-09 12:15:20 +0530

    Time: 31 ms (97.37%) | Memory: 137.7 MB (23.68%) - LeetSync

--- 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
@@ -1,92 +1,169 @@
+import java.util.*;
+
 class Solution {
     public int[] validSequence(String word1, String word2) {
 
         int n = word1.length();
         int m = word2.length();
 
-        char[] w1 = word1.toCharArray();
-        char[] w2 = word2.toCharArray();
-
-        // dp[i] = maximum number of characters
-        // of word2 that can be matched from word1[i...]
-        int[] dp = new int[n + 1];
-
-        int j = m - 1;
-
-        // Build suffix DP
-        for (int i = n - 1; i >= 0; i--) {
-
-            dp[i] = dp[i + 1];
-
-            if (j >= 0 && w1[i] == w2[j]) {
-                dp[i]++;
-                j--;
-            }
-        }
-
-        // If even exact matching is impossible
-        // with at most one mismatch, we will detect it below.
-
         int[] ans = new int[m];
 
-        int i = 0;
+        // last[j] = index in word1 where word2[j] can be matched
+        // while matching word2[j...m-1] as a subsequence.
+        int[] last = new int[m];
+        Arrays.fill(last, -1);
+
+        // ---------------------------------------
+        // Step 1: Build last[] from RIGHT to LEFT
+        // ---------------------------------------
+
+        int i = n - 1;
+        int j = m - 1;
+
+        while (i >= 0 && j >= 0) {
+
+            if (word1.charAt(i) == word2.charAt(j)) {
+                last[j] = i;
+                j--;
+            }
+
+            i--;
+        }
+
+        // ---------------------------------------
+        // Step 2: Greedily build answer LEFT to RIGHT
+        // ---------------------------------------
+
+        boolean canSkip = true; // We have not used our 1 mismatch yet
+
+        i = 0;
         j = 0;
 
         while (i < n && j < m) {
 
-            // Case 1: characters already match
-            if (w1[i] == w2[j]) {
+            // Case 1: Characters match
+            if (word1.charAt(i) == word2.charAt(j)) {
 
                 ans[j] = i;
                 j++;
             }
 
-            // Case 2: characters don't match.
-            // We can use our ONE allowed mismatch here.
-            else {
+            // Case 2: Characters don't match
+            // We can use our ONE allowed mismatch
+            else if (canSkip &&
+                     (j == m - 1 || i < last[j + 1])) {
 
-                /*
-                 * After choosing i as the mismatching character,
-                 * we need to match all remaining characters
-                 * of word2 exactly.
-                 *
-                 * Number of remaining characters:
-                 * m - j - 1
-                 *
-                 * dp[i + 1] tells us how many characters from
-                 * word2's suffix can be matched.
-                 */
-                if (dp[i + 1] >= m - j - 1) {
+                ans[j] = i;
 
-                    ans[j] = i;
-                    j++;
+                j++;
 
-                    // Mismatch has been used.
-                    i++;
-
-                    // Now match the remaining characters EXACTLY.
-                    while (i < n && j < m) {
-
-                        if (w1[i] == w2[j]) {
-                            ans[j] = i;
-                            j++;
-                        }
-
-                        i++;
-                    }
-
-                    break;
-                }
+                // Mismatch is now used
+                canSkip = false;
             }
 
             i++;
         }
 
-        // We couldn't form word2
-        if (j != m) {
-            return new int[0];
+        // If we successfully selected m indices
+        if (j == m) {
+            return ans;
         }
 
-        return ans;
+        // Otherwise no valid sequence exists
+        return new int[0];
     }
 }
+
+
+// class Solution {
+//     public int[] validSequence(String word1, String word2) {
+
+//         int n = word1.length();
+//         int m = word2.length();
+
+//         char[] w1 = word1.toCharArray();
+//         char[] w2 = word2.toCharArray();
+
+//         // dp[i] = maximum number of characters
+//         // of word2 that can be matched from word1[i...]
+//         int[] dp = new int[n + 1];
+
+//         int j = m - 1;
+
+//         // Build suffix DP
+//         for (int i = n - 1; i >= 0; i--) {
+
+//             dp[i] = dp[i + 1];
+
+//             if (j >= 0 && w1[i] == w2[j]) {
+//                 dp[i]++;
+//                 j--;
+//             }
+//         }
+
+//         // If even exact matching is impossible
+//         // with at most one mismatch, we will detect it below.
+
+//         int[] ans = new int[m];
+
+//         int i = 0;
+//         j = 0;
+
+//         while (i < n && j < m) {
+
+//             // Case 1: characters already match
+//             if (w1[i] == w2[j]) {
+
+//                 ans[j] = i;
+//                 j++;
+//             }
+
+//             // Case 2: characters don't match.
+//             // We can use our ONE allowed mismatch here.
+//             else {
+
+//                 /*
+//                  * After choosing i as the mismatching character,
+//                  * we need to match all remaining characters
+//                  * of word2 exactly.
+//                  *
+//                  * Number of remaining characters:
+//                  * m - j - 1
+//                  *
+//                  * dp[i + 1] tells us how many characters from
+//                  * word2's suffix can be matched.
+//                  */
+//                 if (dp[i + 1] >= m - j - 1) {
+
+//                     ans[j] = i;
+//                     j++;
+
+//                     // Mismatch has been used.
+//                     i++;
+
+//                     // Now match the remaining characters EXACTLY.
+//                     while (i < n && j < m) {
+
+//                         if (w1[i] == w2[j]) {
+//                             ans[j] = i;
+//                             j++;
+//                         }
+
+//                         i++;
+//                     }
+
+//                     break;
+//                 }
+//             }
+
+//             i++;
+//         }
+
+//         // We couldn't form word2
+//         if (j != m) {
+//             return new int[0];
+//         }
+
+//         return ans;
+//     }
+// }
