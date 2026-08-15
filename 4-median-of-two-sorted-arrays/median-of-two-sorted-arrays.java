09cccf74fde51c3b59d0d7440c7a8b306034701a
Mayur Hiware <157942713+mayurhiware79@users.noreply.github.com>
2026-08-15 10:46:56 +0530

    Time: 8 ms (13.74%) | Memory: 48.9 MB (52.68%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -30,3 +30,68 @@ class Solution {
         return (arr[total / 2 - 1] + arr[total / 2]) / 2.0;
     }
 }
+
+// OPTIMIZED CODE USING BINARY SERACH FOR CUT
+
+// 1. Binary search the smaller array.
+// 2. Pick a partition in it.
+// 3. Calculate the required partition in the other array.
+// 4. Check: left1 <= right2 AND left2 <= right1.
+// 5. Move binary search left/right until the partition is correct.
+
+// class Solution {
+//     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+//         // Always binary search on the smaller array
+//         if (nums1.length > nums2.length) {
+//             return findMedianSortedArrays(nums2, nums1);
+//         }
+
+//         int m = nums1.length;
+//         int n = nums2.length;
+
+//         int low = 0;
+//         int high = m;
+
+//         while (low <= high) {
+
+//             // Partition of nums1
+//             int cut1 = (low + high) / 2;
+
+//             // Partition of nums2
+//             int cut2 = (m + n + 1) / 2 - cut1;
+
+//             // Elements just left/right of partition
+//             int left1  = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+//             int right1 = (cut1 == m) ? Integer.MAX_VALUE : nums1[cut1];
+
+//             int left2  = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+//             int right2 = (cut2 == n) ? Integer.MAX_VALUE : nums2[cut2];
+
+//             // Correct partition
+//             if (left1 <= right2 && left2 <= right1) {
+
+//                 // Odd total length
+//                 if ((m + n) % 2 == 1) {
+//                     return Math.max(left1, left2);
+//                 }
+
+//                 // Even total length
+//                 return (Math.max(left1, left2)
+//                         + Math.min(right1, right2)) / 2.0;
+//             }
+
+//             // Too many elements taken from nums1
+//             if (left1 > right2) {
+//                 high = cut1 - 1;
+//             }
+
+//             // Too few elements taken from nums1
+//             else {
+//                 low = cut1 + 1;
+//             }
+//         }
+
+//         return 0.0;
+//     }
+// }
